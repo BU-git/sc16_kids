3d5991161d52be493c30a3bb56d49ead200c4f7f
itsmekate <prasolkaterina@gmail.com>
2016-08-12 13:34:51 +0300

    Revert "Validation changes"
    This reverts commit 5dba585f20526d694d2da4a5e0822b6805202baf.

--- src/js/partials/form-validation.js
+++ src/js/partials/form-validation.js
@@ -1,40 +1,4 @@
 $("#phone").mask("+380(99) 999-9999");
 
 
-var validator = $('#contactForm').bootstrapValidator({
-	fields: {
-		name: {
-			message : "Невірно вказані дані",
-			validators: {
-				notEmpty: {
-					message: "Поле обов'язкове для заповнення",
-				},
-				srtingLength: {
-					min: 1,
-					max: 50,
-				},
-			}
-		},
-		email: {
-			message :  "Невірно вказані дані",
-			validators: {
-				notEmpty: {
-					message: "Поле обов'язкове для заповнення",
-				},
-				stringLength: {
-					min: 1,
-					max: 50,
-				},
-				emailAdress: {
-					message: "Невірно вказана електронна адреса",
-				}
-			}
-		},
-		tel: {
-			message: "Невірно вказані дані",
-		},
-		stringLength: {
-					max: 15,
-				},
-	}
-});
+$('form').bootstrapValidator();
