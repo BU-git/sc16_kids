5dba585f20526d694d2da4a5e0822b6805202baf
itsmekate <prasolkaterina@gmail.com>
2016-08-12 12:24:45 +0300

    Validation changes

--- src/js/partials/form-validation.js
+++ src/js/partials/form-validation.js
@@ -1,4 +1,40 @@
 $("#phone").mask("+380(99) 999-9999");
 
 
-$('form').bootstrapValidator();
+var validator = $('#contactForm').bootstrapValidator({
+	fields: {
+		name: {
+			message : "Невірно вказані дані",
+			validators: {
+				notEmpty: {
+					message: "Поле обов'язкове для заповнення",
+				},
+				srtingLength: {
+					min: 1,
+					max: 50,
+				},
+			}
+		},
+		email: {
+			message :  "Невірно вказані дані",
+			validators: {
+				notEmpty: {
+					message: "Поле обов'язкове для заповнення",
+				},
+				stringLength: {
+					min: 1,
+					max: 50,
+				},
+				emailAdress: {
+					message: "Невірно вказана електронна адреса",
+				}
+			}
+		},
+		tel: {
+			message: "Невірно вказані дані",
+		},
+		stringLength: {
+					max: 15,
+				},
+	}
+});
